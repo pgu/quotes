ad3288ec615483afd74fc44362b0e26e02193ae9
pgu <guilcher.pascal@gmail.com>
2015-09-28 18:01:36 +0200

    WIP:

--- quote.js
+++ quote.js
@@ -38,5 +38,61 @@ angular.module('quotesApp', [])
 
     myEventSource.open();
 
-// TODO highcharts
+    $('#container').highcharts('StockChart', {
+
+
+      title: {
+        text: 'GOOG stock price by minute'
+      },
+
+      subtitle: {
+        text: 'Using ordinal X axis'
+      },
+
+      xAxis: {
+        gapGridLineWidth: 0
+      },
+
+      rangeSelector : {
+        buttons : [{
+          type : 'hour',
+          count : 1,
+          text : '1h'
+        }, {
+          type : 'day',
+          count : 1,
+          text : '1D'
+        }, {
+          type : 'all',
+          count : 1,
+          text : 'All'
+        }],
+        selected : 1,
+        inputEnabled : false
+      },
+
+      series : [{
+        name : 'GOOG',
+        type: 'area',
+        data : data,
+        gapSize: 5,
+        tooltip: {
+          valueDecimals: 2
+        },
+        fillColor : {
+          linearGradient : {
+            x1: 0,
+            y1: 0,
+            x2: 0,
+            y2: 1
+          },
+          stops : [
+            [0, Highcharts.getOptions().colors[0]],
+            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+          ]
+        },
+        threshold: null
+      }]
+    });
+
   });
